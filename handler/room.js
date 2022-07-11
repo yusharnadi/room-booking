4dee6b3bb15fb670e97fa5d4f80bcfe0541f9381
yusharnadi <yust.44@gmail.com>
2022-07-11 16:03:02 +0700

    refactor

--- handler/room.js
+++ handler/room.js
@@ -1,27 +1,27 @@
 const db = require('../datastore/init');
 
 const GetAll = (req, res) => {
-  db.query('SELECT * FROM room ORDER BY room_id ASC', (error, result) => {
-    if (error) {
-      res.status(500).json({ status: 'error', message: error.message });
-    }
+    db.query('SELECT * FROM room ORDER BY room_id ASC', (error, result) => {
+        if (error) {
+            res.status(500).json({ status: 'error', message: error.message });
+        }
 
-    res.status(200).json({ status: 'success', data: result.rows });
-  });
+        res.status(200).json({ status: 'success', data: result.rows });
+    });
 };
 
 const GetById = (req, res) => {
-  const id = req.params.id;
-  db.query('SELECT * FROM room WHERE room_id = $1  ORDER BY room_id ASC', [id], (error, result) => {
-    if (error) {
-      res.status(500).json({ status: 'error', message: error.message });
-    }
+    const id = req.params.id;
+    db.query('SELECT * FROM room WHERE room_id = $1  ORDER BY room_id ASC', [id], (error, result) => {
+        if (error) {
+            res.status(500).json({ status: 'error', message: error.message });
+        }
 
-    res.status(200).json({ status: 'success', data: result.rows });
-  });
+        res.status(200).json({ status: 'success', data: result.rows });
+    });
 };
 
 module.exports = {
-  GetAll,
-  GetById,
+    GetAll,
+    GetById,
 };
--- schemaValidator.js
+++ schemaValidator.js
@@ -1,20 +1,20 @@
 // schemas.js
 const Joi = require('joi');
 const schemas = {
-  BookPOST: Joi.object().keys({
-    client: Joi.string().required(),
-    usage_description: Joi.string().required(),
-    room_id: Joi.number().required(),
-  }),
-  userRegister: Joi.object().keys({
-    client: Joi.string().required(),
-    usage_description: Joi.string().required(),
-    room_id: Joi.number().required(),
-  }),
-  userLogin: Joi.object().keys({
-    client: Joi.string().required(),
-    usage_description: Joi.string().required(),
-    room_id: Joi.number().required(),
-  }),
+    BookPOST: Joi.object().keys({
+        client: Joi.string().required(),
+        usage_description: Joi.string().required(),
+        room_id: Joi.number().required(),
+    }),
+    userRegister: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+        role: Joi.string().required(),
+    }),
+    userLogin: Joi.object().keys({
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+    }),
 };
 module.exports = schemas;
