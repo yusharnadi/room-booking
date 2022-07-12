b039ccbb89e5df172dd3f873de36672f92bcd245
yusharnadi <yust.44@gmail.com>
2022-07-12 13:27:27 +0700

    clean up

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const authMiddleware = require('./authMiddleware');
-const jwt = require('jsonwebtoken');
 const router = require('./router');
 
 require('dotenv').config();
--- router.js
+++ router.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const room = require('./handler/room');
 const auth = require('./handler/auth');
 
-router.get('/room', authMiddleware('operator'), room.GetAll);
-router.get('/room/:id', authMiddleware('operator'), room.GetById);
+router.get('/room', authMiddleware('karyawan'), room.GetAll);
+router.get('/room/:id', authMiddleware('karyawan'), room.GetById);
 
-router.get('/book', authMiddleware('operator'), book.GetAll);
-router.get('/book/:id', authMiddleware('operator'), book.GetByID);
-router.post('/book', authMiddleware('operator'), book.Insert);
-router.patch('/book/:id', authMiddleware('operator'), book.Update);
+router.get('/book', authMiddleware('karyawan'), book.GetAll);
+router.get('/book/:id', authMiddleware('karyawan'), book.GetByID);
+router.post('/book', authMiddleware('karyawan'), book.Insert);
+router.patch('/book/:id', authMiddleware('karyawan'), book.Update);
 router.delete('/book/:id', authMiddleware('admin'), book.Delete);
 
 router.post('/auth/register', auth.Register);
